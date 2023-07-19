a8057792e62fc98095765ad3ba8000fe8382d277
tromao <tiagomromao@gmail.com>
2023-07-19 14:02:16 +0100

    added moko resources library

--- build.gradle.kts
+++ build.gradle.kts
@@ -6,6 +6,12 @@ plugins {
     kotlin("multiplatform").apply(false)
 }
 
+buildscript{
+    dependencies{
+        classpath("dev.icerock.moko:resources-generator:0.23.0")
+    }
+}
+
 tasks.register("clean", Delete::class) {
     delete(rootProject.buildDir)
 }
--- shared/build.gradle.kts
+++ shared/build.gradle.kts
@@ -2,6 +2,7 @@ plugins {
     kotlin("multiplatform")
     id("com.android.library")
     id("org.jetbrains.compose")
+    id("dev.icerock.mobile.multiplatform-resources")
 }
 
 //@OptIn(org.jetbrains.kotlin.gradle.ExperimentalKotlinGradlePluginApi::class)
@@ -19,6 +20,7 @@ kotlin {
     targets.withType(org.jetbrains.kotlin.gradle.plugin.mpp.KotlinNativeTarget::class.java).all{
         binaries.withType(org.jetbrains.kotlin.gradle.plugin.mpp.Framework::class.java).all {
             export("dev.icerock.moko:mvvm-core:0.16.1")
+
         }
     }
 
@@ -29,6 +31,8 @@ kotlin {
     ).forEach {
         it.binaries.framework {
             baseName = "shared"
+            export("dev.icerock.moko:resources:0.23.0")
+            export("dev.icerock.moko:graphics:0.9.0")
             isStatic = true
         }
     }
@@ -37,6 +41,7 @@ kotlin {
         val commonMain by getting {
             dependencies {
                 //put your multiplatform dependencies here
+                api("dev.icerock.moko:resources:0.23.0")
                 implementation(compose.runtime)
                 implementation(compose.foundation)
                 implementation(compose.material)
@@ -59,13 +64,20 @@ kotlin {
         val iosX64Main by getting
         val iosArm64Main by getting
         val iosSimulatorArm64Main by getting
-        val iosX64Test by getting
-        val iosArm64Test by getting
         val iosMain by creating {
             dependsOn(commonMain) //make commonMain a dependency of iosMain
             iosX64Main.dependsOn(this)
             iosArm64Main.dependsOn(this)
             iosSimulatorArm64Main.dependsOn(this)
+        }
+        val iosX64Test by getting
+        val iosArm64Test by getting
+        val iosSimulatorArm64Test by getting
+        val iosTest by creating {
+            dependsOn(commonTest) //make commonTest a dependency of iosTest
+            iosX64Test.dependsOn(this)
+            iosArm64Test.dependsOn(this)
+            iosSimulatorArm64Test.dependsOn(this)
         }
     }
 
@@ -90,4 +102,9 @@ dependencies{
     commonMainApi("dev.icerock.moko:mvvm-compose:0.16.1")
     commonMainApi("dev.icerock.moko:mvvm-flow:0.16.1")
     commonMainApi("dev.icerock.moko:mvvm-flow-compose:0.16.1")
+}
+
+multiplatformResources{
+    multiplatformResourcesPackage = "com.example.kmmphillippcontactsproject"
+    multiplatformResourcesClassName = "SharedRes"
 }
