bb1e0250c45308a183a901cb08deb15c85905ac3
tromao <tiagomromao@gmail.com>
2023-07-23 13:04:59 +0100

    Implementation of the Main View Controller to interface with the iOS. Successfully builds and runs the app on the iOS simulator.

---
+++ shared/src/iosMain/kotlin/com/example/kmmphillippcontactsproject/MainViewController.kt
@@ -0,0 +1,15 @@
+package com.example.kmmphillippcontactsproject
+
+import androidx.compose.ui.window.ComposeUIViewController
+import platform.UIKit.UIScreen
+import platform.UIKit.UIUserInterfaceStyle
+
+fun MainViewController() = ComposeUIViewController {
+    val isDarkTheme = UIScreen.mainScreen.traitCollection.userInterfaceStyle ==
+            UIUserInterfaceStyle.UIUserInterfaceStyleDark
+    App(
+        darkTheme = isDarkTheme,
+        dynamicColor = false
+    )
+
+}
